b36efc6404c15ac6b00f42936e5669b05d8f40a4
Jonas Ohland <jonas.ohland@gmail.com>
2020-01-21 09:15:52 +0100

    fix sdps on receive for riedel director

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {cyan, red} from 'chalk';
+import {cyan, green, red} from 'chalk';
 import * as program from 'commander'
 import * as dgram from 'dgram'
 import * as fs from 'fs';
@@ -6,9 +6,70 @@ import * as sdp from 'sdp-transform'
 
 let prog = new program.Command();
 
+let time = Date.now();
+
 prog.option('-i, --interface <interface>', 'listen on this interface');
 prog.option('-d, --out-dir <output directory>', 'put sdp files here');
-prog.option('-k, --keep-files', 'dont delete sdp files when receiving delete messages')
+prog.option(
+    '-k, --keep-files', 'dont delete sdp files when receiving delete messages');
+
+interface AESStreamClock {
+    clksrc: string;
+    clksrcExt: number;
+}
+
+interface AESStreamOrigin {
+    username: string;
+    sessionId: number;
+    sessionVersion: number;
+    netType: string;
+    ipVer: number;
+    address: string;
+}
+
+interface AESStreamConnection {
+    version: number;
+    ip: string;
+}
+
+interface AESStreamRTPDescription {
+    payload: number;
+    codec: string;
+    rate: number;
+    encoding: number;
+}
+
+interface AESStreamMedia {
+    rtp: AESStreamRTPDescription[];
+    type: string;
+    port: 5004;
+    protocol: string;
+    payloads: 97;
+    description: string;
+    direction: string;
+    ptime: number;
+    tsRefClocks: AESStreamClock[];
+}
+
+interface AESStreamSDP {
+    name: string;
+    origin: AESStreamOrigin;
+    connection: AESStreamConnection;
+    media: AESStreamMedia[];
+    keywords?: string;
+}
+
+class Service {
+
+    _timeout: NodeJS.Timeout;
+    _id: string;
+    _name: string;
+
+    _sdp_string: string;
+    _sdp: AESStreamSDP;
+
+    constructor() {}
+}
 
 function _BV(bit: number)
 {
@@ -52,23 +113,47 @@ enum SAPHeaderBits {
     COMPRESSED = _BV(0)
 }
 
+function fix_payload(payload: string) {
+
+    let lines = payload.split(/\r\n/);
+
+    if(lines.findIndex(line => line.startsWith('v')) == -1)
+        lines.unshift('v=0');
+
+    let cidx = lines.findIndex(line => line.startsWith('c'));
+
+    if(cidx == -1)
+        return;
+
+    let cline = lines.splice(cidx, 1)[0];
+    
+    let midx = lines.findIndex(line => line.startsWith('m'));
+
+    if(midx == -1)
+        return;
+
+    lines.splice(midx, 0, cline);
+
+    return lines.join('\r\n');
+}   
+
 const receiverSocket = dgram.createSocket('udp4');
 
 receiverSocket.on('message', data => {
-    
-    let header   = data.readUInt8(0);
+    let header = data.readUInt8(0);
 
     let options = {
-        addr_type : (header & SAPHeaderBits.ADDR_TYPE) == SAPHeaderBits.ADDR_TYPE,
-        msg_type : (header & SAPHeaderBits.MSG_TYPE) == SAPHeaderBits.MSG_TYPE,
-        encypted : (header & SAPHeaderBits.ENCRYPTED) == SAPHeaderBits.ENCRYPTED,
-        compressed : (header & SAPHeaderBits.COMPRESSED) == SAPHeaderBits.COMPRESSED,
+        addr_type : (header & SAPHeaderBits.ADDR_TYPE) > 0,
+        msg_type : (header & SAPHeaderBits.MSG_TYPE) > 0,
+        encypted : (header & SAPHeaderBits.ENCRYPTED) > 0,
+        compressed : (header & SAPHeaderBits.COMPRESSED) > 0,
         auth_len : data.readUInt8(1),
         msg_id_hash : data.readUInt16LE(2)
     };
 
     if (options.encypted || options.compressed)
-        return console.log('Could not decode received message');
+        return console.log(
+            'Could not decode received message (payload encrypted or compressed)');
 
     let source_addr = data.readUInt32LE(4);
 
@@ -86,25 +171,36 @@ receiverSocket.on('message', data => {
 
     let out_file = `${(prog.outDir) ? prog.outDir + '/' : ''}${sdptitle}.sdp`;
 
-    console.log(`Received SAP Message ${
-        options.msg_type ? '[RMV]' : '[ADD]'} ${cyan(sdptitle)} from ${
-        cyan(addrToString(source_addr))}`);
+    console.log(`Received SAP Message ${options.msg_type ? '[RMV]' : '[ADD]'} ${
+        cyan(sdptitle)} from ${cyan(addrToString(source_addr))}`);
 
-    if (options.msg_type) {
-        if (!(prog.keepFiles) && fs.existsSync(out_file)) fs.unlinkSync(out_file);
+    let stream = <AESStreamSDP>sdp_obj;
+
+    let payload_str = fix_payload(payload.toString());
+
+    if(payload_str) {
+
+        if (options.msg_type) {
+            if (!(prog.keepFiles) && fs.existsSync(out_file))
+                fs.unlinkSync(out_file);
+        }
+        else
+            fs.writeFileSync(out_file, payload_str);
+
+        let ntime = Date.now();
+
+        time = ntime;
     }
-    else
-        fs.writeFileSync(out_file, payload);
 })
 
 receiverSocket.on('error', error => {
     console.log(error);
 });
 
-console.log('Binding...');
+process.stdout.write(`Binding to port ${cyan('9875')}...`);
 
 receiverSocket.bind(9875, () => {
-    console.log('Bound')
+    console.log(green(' done'));
     console.log(`Add multicast membership to ${cyan('239.255.255.255')}`);
     receiverSocket.addMembership('239.255.255.255', prog.interface);
 });
